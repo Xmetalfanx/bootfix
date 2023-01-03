367acabb29e45da1c2c562bc320d6cc1316c5011
Xmetalfanx <Xmetalfanx@yahoo.com>
2023-01-02 22:40:13 -0500

    code refactoring

--- bootfix2204.sh
+++ bootfix2204.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 clear
 
 function userPrompt() {
@@ -5,47 +7,53 @@ function userPrompt() {
 }
 
 function checkStatus() {
-  [ $? -eq 0 ] && echo "${1}" || echo "${2}"
-
-  userPrompt
+  # not sure if this is right atm but exiting in case of a error
+  [ $? -eq 0 ] && echo "${1}" && userPrompt || echo "${2}" || userPrompt || exit
 }
 
 # source https://ubuntu-mate.community/t/set-sis-graphics-1024x768-l-ubuntu-mate-14-04-and-16-04-16-10/3553
 # not sure if this is a great idea but on my old desktop (the point of these scripts) this helps boot directly
 # to a GUI where i dont have to do anything on bootup
-function useVesa() {
-  
+function createUseVesaFile() {
+
   vesaFile="use-vesa.conf"
-  # where to put the file - a dir/folder
-  useVesaFinalDir="/usr/share/x11/xorg.conf.d/"
 
   echo -e "Adding file for VESA drivers"
-  cat << EOF > ${vesaFile}
-  Section "Device"
-      Identifier "Configured Video Device"
-      Driver "vesa"
-  EndSection
-  # is the tab level of the above correct?
+
+  cat << EOF > "${vesaFile}"
+Section "Device"
+  Identifier "Configured Video Device"
+  Driver "vesa"
+EndSection
+EOF
 
   checkStatus "Vesa file successfully created" "Error creating Vesa file"
 
-  # NOTE: the above code just creates a local file (so to speak), and the code below moves it to
-  # where it needs to be ... this will help me check the file is correctly made
+  clear
+  cat ${vesaFile}
 
-  ###################################################################################################
-  #echo -e "Copying ${vesaFile} to proper location"
-  #sudo cp ${vesaFile} ${useVesaFinalDir}
+}
 
-  #checkStatus "File Moved Successfully" "File Move Failed"
+# Move the newly created vesa file to the proper location
+function moveUseVesaFile() {
+
+  # where to put the file - a dir/folder
+  useVesaFinalDir="/usr/share/x11/xorg.conf.d/"
+
+  echo -e "Copying ${vesaFile} to proper location"
+  sudo cp ${vesaFile} ${useVesaFinalDir}
+
+  checkStatus "File Moved Successfully" "File Move Failed"
   userPrompt
 }
 
-#useVesa
+createUseVesaFile
+#moveUseVesaFile
 
 #################################################################################
+# for nomodeset addition
 
-# To Add nomodeset
-function addNoModeSet() {
+function backupGrubDefaultFile() {
 
   # grub configuration file
   defaultGrubFile="/etc/default/grub"
@@ -54,25 +62,29 @@ function addNoModeSet() {
   searchTerm="GRUB_CMDLINE_LINUX_DEFAULT"
 
   # make backup grub config file from the start
-  echo -e "Making backup of default grub config file
+  echo -e "Making backup of default grub config file"
   sudo cp ${defaultGrubFile} /etc/default/grub_backup
 
   checkStatus "Grub Default config file created" "Error creating grub default backup file"
 
+}
+
+# To Add nomodeset
+function addNoModeSet() {
 
   # original line that has to be modified
   ORIGINAL_LINE=$(grep -E "^${searchTerm}" ${defaultGrubFile})
 
   # Modified Line
-  NEW_LINE=$(echo ${ORIGINAL_LINE} | sed 's/"/ nomodeset"/2')
+  NEW_LINE=$(echo "${ORIGINAL_LINE}" | sed 's/"/ nomodeset"/2')
 
   echo -e "Adding nomodeet to needed line"
   sed "s/${ORIGINAL_LINE}/${NEW_LINE}/" ${defaultGrubFile} > test.txt
 
   checkStatus "Test File created successfully" "Error Creating Test File"
 
-
-
 }
 
-addNoModeSet
+#backupGrubDefaultFile
+
+#addNoModeSet
